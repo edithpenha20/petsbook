235862563856c52f2c05da2a89a9a7cd91d28559
Penha <edith.penha@capgemini.com>
2022-09-11 08:18:50 -0300

    curso boas praticas concluido

--- src/app/componentes/rodape/rodape.module.ts
+++ src/app/componentes/rodape/rodape.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RodapeComponent } from './rodape.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,11 @@ import { RodapeComponent } from './rodape.component';
     RodapeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    RodapeComponent
   ]
 })
 export class RodapeModule { }
